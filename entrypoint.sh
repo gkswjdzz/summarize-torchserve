033415f7eb5b8299f51277e0ec74f896bcf8a03d
Hanjeong Lee <gkswjdzz@naver.com>
2021-06-21 17:25:28 +0900

    add bart large xsum model

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 curl -X GET $BART_BASE_CNN_LINK -o /home/model-server/model-store/bart-base-cnn.mar
 curl -X GET $BART_BASE_XSUM_LINK -o /home/model-server/model-store/bart-base-xsum.mar
 curl -X GET $BART_LARGE_BASE_CNN_LINK -o /home/model-server/model-store/bart-large-base-cnn.mar
+curl -X GET $BART_LARGE_BASE_XSUM_LINK -o /home/model-server/model-store/bart-large-base-xsum.mar
 
 torchserve --start --ncs --model-store=/home/model-server/model-store --models=all
