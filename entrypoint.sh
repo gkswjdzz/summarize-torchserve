ff1d9d6eb7a0cc5848e734cd25874b6f5cc0ed82
Hanjeong Lee <gkswjdzz@naver.com>
2021-06-29 02:41:02 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ curl -X GET $KOBART_LINK -o /home/model-server/model-store/kobart.mar
 nginx -c /app/nginx.conf
 nginx -s reload
 torchserve --start --ncs --model-store=/home/model-server/model-store --ts-config /home/model-server/config.properties
-sleep 10
+sleep 100
 ls /home/model-server/model-store > model_list.txt
 cat model_list.txt | xargs -I {} curl -X POST http://localhost:8081/models?url={}
 streamlit run /streamlit/summarize.py --server.enableXsrfProtection=false --server.enableCORS=true
