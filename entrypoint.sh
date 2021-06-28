3b8ac441bab6225ec85b5c7a736519e6998614f4
Hanjeong Lee <gkswjdzz@naver.com>
2021-06-29 01:14:43 +0900

    register model to torchserve sequentially

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,8 @@ curl -X GET $KOBART_LINK -o /home/model-server/model-store/kobart.mar
 
 nginx -c /app/nginx.conf
 nginx -s reload
-torchserve --start --ncs --model-store=/home/model-server/model-store --models=all --ts-config /home/model-server/config.properties
+torchserve --start --ncs --model-store=/home/model-server/model-store --ts-config /home/model-server/config.properties
+sleep 10
+ls /home/model-server/model-store > model_list.txt
+cat model_list.txt | xargs -I {} curl -X POST http://localhost:8081/models?url={}
 streamlit run /streamlit/summarize.py --server.enableXsrfProtection=false --server.enableCORS=true
